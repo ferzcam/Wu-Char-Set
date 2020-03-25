f02c48635d0d3e59c708e27ba4faa123a1b8041c
anthonyramos47 <anthonyramos47@gmail.com>
2020-03-25 10:30:03 -0500

    Test Apolonio Pascal Morley

--- test-suite/TPolynomial/TWu.hs
+++ test-suite/TPolynomial/TWu.hs
@@ -70,28 +70,28 @@ import Polynomial.Prelude
 
 
 ---------------------------------Morleys  THEOREM----------------------------------
--- y1 = var 9
--- y2 = var 8 
--- y3 = var 7
--- y4 = var 6
--- y5 = var 5
--- y6 = var 4
--- y7 = var 3
--- y8 = var 2
--- y9 = var 1
--- y10 = var 0
+y1 = var 9
+y2 = var 8 
+y3 = var 7
+y4 = var 6
+y5 = var 5
+y6 = var 4
+y7 = var 3
+y8 = var 2
+y9 = var 1
+y10 = var 0
 
 
--- hh1, hh2, hh3, hh4, hh5, hh6, hh7, g :: Polynomial' Grevlex 10
--- --h1,h2,h3,h4,h5,h6,h7,h8,h9,h10,g
--- hh1 = (y3^3 + ((-1)*3*y2^3 + 6*y1*y2 - 3*y1^2 )*y3 )*y5 + ((3*y1 - 3*y2)*y3^2 + y2^3 - 3*y2*y2^2 + 3*y1^2*y2 -y1^3)*y4 - y1*y3^3 + (3*y1*y2^2 - 6*y1^2*y2 + 3*y1^3)*y3
--- hh2 = (y3^3 - 3*y2^2*y3)*y5 + (y2^3 - 2*y2*y3^2)*y4
--- hh3 = y6^2 - 3 
--- hh4 = (((y3^2 + y2^2 - y1*y2)*y5 - y1*y3*y4)*y6 + y1*y3*y5 + (y3^2 +y2^2 - y1*y2)*y4)*y8  + ((y1*y3*y5 + (y3^2 + y2^2 - y1*y2)*y4)*y6 - (y3^2 + y2^2 - y1*y2)*y5 + y1*y2*y4)*y7 - ((y3^2 + y2^2 - y1*y2)*y5^2 + (y3^2 + y2^2 - y1*y2)*y4^2)*y6 - y1*y3*y5^2 - y1*y3*y4^2  
--- hh5 = (y1*y3*y5 - y1*y2*y4)*y8 + (y1*y2*y5 + y1*y3*y4)*7
--- hh6 = (y1*y3*y5 + (y1^2 - y1*y2)*y4 - y1^2*y3)*y10 + ((y1*y2 - y1^2)*y5 + y1*y2*y4 - y1^2*y2 + y1^3)*y9 - y1^2*y2*y5 + (y1^2*y2 - y1^3)*y4 + y1^3 * y3 
--- hh7 = ((2*y4*y5 - y1*y4)*y8 + (y4^2 + y1*y5 - y5^2)*y7 - y4*y5^2 - y4^3)*y10 + ((y4^2 + y1*y5 - y5^2)*y8 + (y1*y4 - 2*y4*y5)*y7 + y5^3 - y1*y5^2 + y4^2*y5 -y1*y4^2)*y9 - (y4^3 + y4*y5^2)*y8 + (y5^3 - y1*y5^2 + y4^2*y5 - y1*y4^2)*y7 + y1*y4*y5^2 + y1*y4^3
--- g = (y6*y8 - y7 -y2*y6 + y3)*y10 + (y8 + y8*y7 - y3*y6 - y2)*y9 - (y3 + y2*y6 )*y8 + (y2 - y3*y6 )*y7 + (y3^2 + y2^2 )*y6
+hh1, hh2, hh3, hh4, hh5, hh6, hh7, g :: Polynomial' Grevlex 10
+--h1,h2,h3,h4,h5,h6,h7,h8,h9,h10,g
+hh1 = (y3^3 + ((-1)*3*y2^3 + 6*y1*y2 - 3*y1^2 )*y3 )*y5 + ((3*y1 - 3*y2)*y3^2 + y2^3 - 3*y2*y2^2 + 3*y1^2*y2 -y1^3)*y4 - y1*y3^3 + (3*y1*y2^2 - 6*y1^2*y2 + 3*y1^3)*y3
+hh2 = (y3^3 - 3*y2^2*y3)*y5 + (y2^3 - 2*y2*y3^2)*y4
+hh3 = y6^2 - 3 
+hh4 = (((y3^2 + y2^2 - y1*y2)*y5 - y1*y3*y4)*y6 + y1*y3*y5 + (y3^2 +y2^2 - y1*y2)*y4)*y8  + ((y1*y3*y5 + (y3^2 + y2^2 - y1*y2)*y4)*y6 - (y3^2 + y2^2 - y1*y2)*y5 + y1*y2*y4)*y7 - ((y3^2 + y2^2 - y1*y2)*y5^2 + (y3^2 + y2^2 - y1*y2)*y4^2)*y6 - y1*y3*y5^2 - y1*y3*y4^2  
+hh5 = (y1*y3*y5 - y1*y2*y4)*y8 + (y1*y2*y5 + y1*y3*y4)*7
+hh6 = (y1*y3*y5 + (y1^2 - y1*y2)*y4 - y1^2*y3)*y10 + ((y1*y2 - y1^2)*y5 + y1*y2*y4 - y1^2*y2 + y1^3)*y9 - y1^2*y2*y5 + (y1^2*y2 - y1^3)*y4 + y1^3 * y3 
+hh7 = ((2*y4*y5 - y1*y4)*y8 + (y4^2 + y1*y5 - y5^2)*y7 - y4*y5^2 - y4^3)*y10 + ((y4^2 + y1*y5 - y5^2)*y8 + (y1*y4 - 2*y4*y5)*y7 + y5^3 - y1*y5^2 + y4^2*y5 -y1*y4^2)*y9 - (y4^3 + y4*y5^2)*y8 + (y5^3 - y1*y5^2 + y4^2*y5 - y1*y4^2)*y7 + y1*y4*y5^2 + y1*y4^3
+g = (y6*y8 - y7 -y2*y6 + y3)*y10 + (y8 + y8*y7 - y3*y6 - y2)*y9 - (y3 + y2*y6 )*y8 + (y2 - y3*y6 )*y7 + (y3^2 + y2^2 )*y6
 ---------------------------------Termina Morley Theorem---------------------------------------------------
 
 
@@ -119,9 +119,9 @@ import Polynomial.Prelude
 testTheorem :: TestTree
 testTheorem = testCase "Test for Theorem Prover" $ do
     -- Pascal
-    last (theoremProver [h1,h2,h3,h4,h5,h6,h7,h8,h9,h10] g) @?=  0
+    -- last (theoremProver [h1,h2,h3,h4,h5,h6,h7,h8,h9,h10] g) @?=  0
     -- MOrley
-    -- last (theoremProver [hh1,hh2,hh3,hh4,hh5,hh6,hh7] g) @?=  0
+    last (theoremProver [hh1, hh2, hh3, hh4, hh5, hh6, hh7] g) @?=  0
     --last (theoremProver [h1,h2,h3,h4,h5,h6,h7,h8,h9,h10] g) @?=  0
 --testReplacePoly :: TestTree
 

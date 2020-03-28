f4d5d5f33d7852e055d6b60bcb3153eb7ab7aa93
anthonyramos47 <anthonyramos47@gmail.com>
2020-03-28 16:06:46 -0500

    Paralle, Perpendicular, InCircle, Samelen, Acute

--- executable/Main.hs
+++ executable/Main.hs
@@ -25,18 +25,21 @@ lda = Line d a
 lbc = Line b c 
 lao = Line a o
 ldo = Line d o
+angl_cbi = Angle c b o
+angl_iba = Angle o b a
 
 h1 = Parallel lab ldc
-h2 = Parallel lda lbca
+h2 = Parallel lda lbc
 h3 = Colinear d o b
 h4 = Colinear a c o
+h5 = Acute angl_cbi angl_iba
 
 conc = SameLen lao ldo
 
-structures = [P a, P b, P c, P d, P o, L lab, L ldc, L lac, L lbd, L lda, L lbc, L lao, L ldo]
+structures = [P a, P b, P c, P d, P o, L lab, L ldc, L lac, L lbd, L lda, L lbc, L lao, L ldo, A angl_cbi, A angl_iba]
 
 res :: [Polynomial' Grevlex 10]
-res = generatePolynomials structures [h1, h2, h3, h4] conc
+res = generatePolynomials structures [h1, h2, h3, h4, h5] conc
 
 
 main :: IO ()
--- library/Util/Tokenizer.hs
+++ library/Util/Tokenizer.hs
@@ -12,20 +12,22 @@ import Debug.Trace
 import Data.Maybe
 
 newtype Point = Point String deriving (Eq, Show)
-data Circle = Circle (Point, Point) Point deriving (Show) 
+data Circle = Circle Point Point deriving (Show) 
 data Line = Line Point Point deriving (Show)
+data Angle = Angle Point Point Point deriving (Show)
 
 data Struct =   P {getPoint :: Point}
                 | C Circle 
                 | L Line
+                | A Angle
             deriving (Show)
 
 data Hypothesis =   Colinear Point Point Point 
                     | Parallel Line Line 
                     | Perpendicular Line Line 
-                    | Intersect Line Line
                     | InCircle Point Circle
                     | SameLen Line Line
+                    | Acute Angle Angle
 
 type Conclusion = Hypothesis
 
@@ -73,14 +75,37 @@ geomToAlg (Parallel l1 l2) var =
                                     p4 = pts!!3
                                 in  (p2!!1 - p1!!1)*(p4!!0 - p3!!0) - (p4!!1 - p3!!1)*(p2!!0 - p1!!0) 
 geomToAlg (SameLen (Line pt1 pt2) (Line pt3 pt4)) var =  
+                                let dist12 = distance pt1 pt2 var
+                                    dist34 = distance pt3 pt4 var
+                                in  dist12 - dist34
+geomToAlg (Perpendicular (Line pt1 pt2) (Line pt3 pt4)) var =  
                                 let pts = map (\point -> fromJust $ lookup point var ) $ [pt1, pt2, pt3, pt4]
-                                    p1 = trace ("pts:" ++ show pts) pts!!0
+                                    p1 = pts!!0
                                     p2 = pts!!1
                                     p3 = pts!!2
                                     p4 = pts!!3
-                                in  (p2!!1 - p1!!1)^2 + (p2!!0 - p1!!0)^2 - (p4!!0 - p3!!0)^2 - (p4!!1 - p3!!1)^2  
+                                in  (p2!!1 - p1!!1)*(p4!!1 - p3!!1) + (p4!!0 - p3!!0)*(p2!!0 - p1!!0)
+geomToAlg (InCircle (pt1) (Circle pt2 pt3)) var = 
+                                let dist23 = distance pt2 pt3 var
+                                    dist21 = distance pt2 pt1 var
+                                in dist23 - dist21
+geomToAlg (Acute (Angle pt1 pt2 pt3) (Angle pt4  pt5 pt6)) var =  
+                                let pts = map (\point -> fromJust $ lookup point var ) $ [pt1, pt2, pt3, pt4, pt5, pt6]
+                                    p1 = trace("pts :" ++ show pts) pts!!0
+                                    p2 = pts!!1
+                                    p3 = pts!!2
+                                    p4 = pts!!3
+                                    p5 = pts!!4
+                                    p6 = pts!!5
+                                in  ((p3!!1 - p2!!1)*(p1!!0 - p2!!0) - (p1!!1 - p2!!1)*(p3!!0 - p2!!0)  )*((p4!!0 - p5!!0)*(p6!!0 - p5!!0)  + (p4!!1 - p5!!1)*(p6!!1 - p5!!1)  )  -  ((p6!!1 - p5!!1)*(p4!!0 - p5!!0) - (p4!!1 - p5!!1)*(p6!!0 - p5!!0))* ((p1!!0 - p2!!0)*(p3!!0 - p2!!0)  + (p1!!1 - p2!!1)*(p3!!1 - p2!!1)  ) 
+ 
 
-
+distance :: (KnownNat n) => Point -> Point -> [(Point, [Polynomial' Grevlex n])] -> Polynomial' Grevlex n
+distance pt1 pt2 var = (p2!!1 - p1!!1)^2 + (p2!!0 - p1!!0)^2
+                where 
+                    pts = map (\point -> fromJust $ lookup point var ) $ [pt1, pt2]
+                    p1 = pts!!0
+                    p2 = pts!!1
 
 
 uncurry3 :: (a -> b -> c -> d) -> (a,b,c) -> d
@@ -88,15 +113,14 @@ uncurry3 f (a,b,c) = f a b c
 
 instance Flattening Struct where
     flatten (P point) = [point] 
-    flatten (C (Circle (h,k) r)) = [h,k,r]
+    flatten (C (Circle c r)) = [c,r]
     flatten (L (Line p1 p2)) = [p1,p2]
 
 instance Flattening Hypothesis where
     flatten (Colinear p1 p2 p3) = nub [p1, p2, p3] 
     flatten (Parallel (Line p1 p2) (Line p3 p4)) = nub [p1, p2, p3, p4] 
     flatten (Perpendicular (Line p1 p2) (Line p3 p4)) = nub [p1, p2, p3, p4] 
-    flatten (Intersect (Line p1 p2) (Line p3 p4)) = nub [p1, p2, p3, p4] 
-    flatten (InCircle p (Circle (h,k) r)) = nub [p, h, k, r]
+    flatten (InCircle p (Circle c r)) = nub [p, c, r]
     flatten (SameLen (Line p1 p2) (Line p3 p4)) = nub [p1, p2, p3, p4] 
 
 
